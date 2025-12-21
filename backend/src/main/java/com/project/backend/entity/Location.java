82b077df4b283a3874388c12aa93a03636b893ed
codeweavers26-gif <codeweavers26@gmail.com>
2025-12-21 15:47:39 +0530

    docker
    docker

--- backend/src/main/java/com/project/backend/entity/Location.java
+++ backend/src/main/java/com/project/backend/entity/Location.java
@@ -1,17 +1,21 @@
 package com.project.backend.entity;
-import jakarta.persistence.Id;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Table(name = "locations")
 @Getter @Setter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Location {
 
     @Id
