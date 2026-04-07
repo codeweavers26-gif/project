e7c08c85d392c1068c6c93eaf05384991fa64180
priya gupta <guptapriya246@gmail.com>
2026-04-07 14:06:14 +0530

    app

--- backend/src/main/java/com/project/backend/service/EmailService.java
+++ backend/src/main/java/com/project/backend/service/EmailService.java
@@ -5,9 +5,11 @@ import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class EmailService {
 private final JavaMailSender mailSender;
 
@@ -27,6 +29,7 @@ private final JavaMailSender mailSender;
             mailSender.send(message);
 
         } catch (Exception e) {
+            log.error("Email. failed error",e);
             throw new RuntimeException("Failed to send email OTP", e);
         }
     }
