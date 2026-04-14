2aa3706feedfd97b9f8b1a9e7fad39d2d6cdde86
Piyush Yadav <yadavpiyush045@gmail.com>
2026-04-15 02:03:14 +0530

    fix prdt  endpoint and bugs

--- backend/src/main/java/com/project/backend/service/ProductService.java
+++ backend/src/main/java/com/project/backend/service/ProductService.java
@@ -139,11 +139,9 @@ public class ProductService {
 		Product product = Product.builder().name(dto.getName()).brand(dto.getBrand())
 				.shortDescription(dto.getShortDescription()).description(dto.getDescription()).category(category)
 				.isActive(true).isDeleted(false)
-				//.price(dto.getPrice())
+				.price(0.0)
 				.deliveryDays(dto.getDeliveryDays())
-				//.mrp(dto.getMrp())
 				.codAvailable(dto.getCodAvailable())
-			//	.discountPercent(dto.getDiscountPercent())
 				.taxPercent(dto.getTaxPercent())
 				.weight(dto.getWeight()).length(dto.getLength()).width(dto.getWidth()).height(dto.getHeight())
 				.stock(dto.getStock() != null ? dto.getStock() : 0).build();
